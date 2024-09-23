9c997a75e6041ebca75bada3edac0f10e8ac3ec6
emeraldloop <sergey.nikishin.2000@mail.ru>
2024-09-23 20:10:51 +0300

    удалил пример со старой либы

--- Model3dViewerTest/Program.cs
+++ Model3dViewerTest/Program.cs
@@ -1,26 +1,10 @@
-﻿using Assimp;
-using Assimp.Configs;
-using static System.Formats.Asn1.AsnWriter;
+﻿
 
 class Program
 {
     static void Main(string[] args)
     {
-        string path = "D:\\PROGA\\C-Mehatronics\\Cube1x1x1.STEP";
-        AssimpContext importer = new AssimpContext();
 
-        Scene scene = importer.ImportFile(path, PostProcessPreset.TargetRealTimeMaximumQuality);
-
-        // Проверка поддержки формата
-        if (importer.IsImportFormatSupported(".STEP"))
-        {
-            //Scene scene = importer.ImportFile(path, PostProcessPreset.TargetRealTimeMaximumQuality);
-            Console.WriteLine("STEP file loaded successfully.");
-        }
-        else
-        {
-            Console.WriteLine("STEP format is not supported.");
-        }
     }
 }
 
